bbdd94c754e934c369d3b799aabbf2a15f42e723
huanghuang240@163.com <qq198902222614>
2015-02-03 20:17:01 +0800

    fix domcontentloaded bug

--- dist/mping.js
+++ dist/mping.js
@@ -1055,50 +1055,7 @@
         hex_md5 : hex_md5
     };
 
-}(window));;(function(MPing, window, undefined) {
-    var _init = false,
-        _iframe = null,
-        _ready = false,
-        _handler = [],
-        _core = null;
-
-    MPing.tools.localShare = function(fn) {
-        if (false === _init) {
-            MPing.tools.localShare.onComplete = function() {
-                _core = _iframe.contentWindow.storageCore;
-                for (var i = 0, len = _handler.length; i < len; i++) {
-                    typeof _handler[i] === "function" && _handler[i].call(_core)
-                }
-                _ready = true;
-                _handler = null;
-                _iframe = null;
-                delete MPing.tools.localShare.onComplete
-            };
-
-            //var _prefix = ('https:' == document.location.protocol) ? G.prefix.st_ssl : G.prefix.st;
-            _iframe = document.createElement("IFRAME");
-            _iframe.src = 'http://h5.m.jd.com/active/track/proxy.html?v=xxxx';
-            _iframe.style.display = "none";
-            _init = true;
-            document.body.insertBefore(_iframe, document.body.firstChild);
-        }
-
-        if (_ready) {
-            typeof fn === "function" && fn.call(_core);
-        } else {
-            _handler.push(fn);
-        }
-    };
-
-    //预加载
-    var root = document.querySelector('body');
-
-    root.addEventListener("DOMContentLoaded", function(e) {
-        MPing.tools.localShare();
-    }, false);
-
-})(MPing, window);
-;/**
+}(window));;/**
  * @fileoverview 这个文件是所有事件id与事件等级对应表
  */
 
