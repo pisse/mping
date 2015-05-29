09fff928ab5575ad198d8431bbd90585d9f4dbf8
huanghuang240@163.com <qq198902222614>
2015-05-29 19:03:01 +0800

    add pv_sid pv_seq with app

--- dist/mping.js
+++ dist/mping.js
@@ -178,6 +178,7 @@
 
             //内嵌页
             if(tools.isEmbedded()){
+                var mcookie = new MCookie();
                 this.options.pv_sid = mcookie.getSid();
                 this.options.pv_seq = mcookie.getSeq();
             }
@@ -791,6 +792,8 @@
             MCookie._instance = this;
             this.initialize();
             return MCookie._instance;
+        }else{
+            return MCookie._instance;
         }
 
         var _mbaMuidSeq,
@@ -806,7 +809,6 @@
             this.setSid();
             return _mbaSidSeq[0];
         };
-
         //读取mba_seq
         this.getSeq = function(){
             this.setSid();
@@ -816,20 +818,15 @@
         this.setMuid = function(){
             if(!tools.getCookie("mba_muid")){
                 _mbaMuidSeq[0] = tools.getUniq();
-                _mbaMuidSeq[1] = 1;
             }else {
-                _mbaMuidSeq = tools.getCookie("mba_muid").split(".");
-                if(_mbaMuidSeq[1]==undefined){
-                    _mbaMuidSeq[1]=1;
-                }
-                _mbaMuidSeq[1] = tools.getCookie("mba_sid") ? _mbaMuidSeq[1] : (_mbaMuidSeq[1]*1+1);
+                _mbaMuidSeq[0] = tools.getCookie("mba_muid").split(".")[0];
             }
             this.setMuidCookie();
         };
         this.setSid = function( type){
             //内嵌页使用app带过来的pv_sid,pv_seq
             if(tools.isEmbedded()){
-                this.setPVSid();
+                this.setPVSid(type);
                 return;
             }
 
@@ -844,15 +841,45 @@
             this.setSidCookie();
         };
 
-        this.setPVSid = function(){
+        this.setPVSid = function(type){
             var ua =  navigator.userAgent,
-                app_sid_seq_flag = 'pv_sid/',
-                app_sid_seq;
-            if( ua.indexOf("pv/")>-1 ){
-                var endIdx = ua.indexOf(";", ua.indexOf("pv/")>-1 );
-                app_sid_seq = ua.substring(ua.indexOf(app_sid_seq_flag) + app_sid_seq_flag.length, 1)
+                app_sid_seq_index = ua.indexOf('pv/'),
+                app_sid_seq, //来自app
+                cookie_sid_seq,//来自cookie,
+                pv_sid;
+            if( app_sid_seq_index > -1 ){
+                var endIdx = ua.indexOf(";", app_sid_seq_index );
+                if(endIdx < 0){
+                    endIdx = ua.length;
+                }
+                app_sid_seq = ua.substring(app_sid_seq_index + 3, endIdx);
+            }else{
+                app_sid_seq = '1.0';
             }
-            var pv_sid_seq = ua.substring(ua.indexOf("pv_sid/"))
+
+
+            if(tools.getCookie("mba_sid")){
+                cookie_sid_seq = tools.getCookie("mba_sid");
+            } else{
+                var m_muid = tools.getCookie("mba_muid") ,m_muid_arr =  m_muid.split(".");//？关闭app，m_muid不清除？
+                if(m_muid_arr.length == 3){
+                    var pre_sid = m_muid[1], pre_timestamp = parseInt(m_muid[2]);
+                    if(new Date().getTime() - pre_timestamp > Options.MCookie.sessionCookieTimeout ){
+                        cookie_sid_seq = [pre_sid, 0].join(".");
+                    }
+                } else{
+                    cookie_sid_seq = '1.0';
+                }
+            }
+            pv_sid = app_sid_seq> cookie_sid_seq ? app_sid_seq : cookie_sid_seq;
+            _mbaSidSeq[0] = pv_sid.split(".")[0];
+            _mbaSidSeq[1] = (pv_sid.split(".")[1] ? pv_sid.split(".")[1]: 1)*1  + (type==="pv" ? 1 : 0) ;
+
+            _mbaMuidSeq[1] = _mbaSidSeq[0];
+            _mbaMuidSeq[2] = new Date().getTime();
+
+            this.setSidCookie();
+            this.setMuidCookie();
         }
 
         this.setMuidCookie = function(){
@@ -899,6 +926,219 @@
             return MPing;
         });
     }*/
+
+}(window));;;(function(window){
+
+    (function(w, undefined) {
+        if (w.localStorage !== undefined && !(window.attachEvent && navigator.userAgent.indexOf('Opera') === -1)) {
+            w.mbaShareCore = w.localStorage;
+            return;
+        }
+
+        function UserData(file) {
+            this.dom = document.getElementById("share_core");
+            this.file = file || "user_data_default";
+        };
+
+        UserData.prototype = {
+            setItem: function(k, v) {
+                this.dom.setAttribute(k, v);
+                this.dom.save(this.file);
+            },
+
+            getItem: function(k) {
+                try { // 处理文件可能被QQ管家之类的东东删掉的情况
+                    this.dom.load(this.file);
+                }
+                catch(e) {
+                    return null;
+                }
+                return this.dom.getAttribute(k);
+            },
+
+            removeItem: function(k) {
+                this.dom.removeAttribute(k);
+                this.dom.save(this.file);
+            },
+
+            clear: function() {
+                try { // 处理文件可能被QQ管家之类的东东删掉的情况
+                    this.dom.load(this.file);
+                    now = new Date(new Date().getTime() - 1);
+                    this.dom.expires = now.toUTCString();
+                    this.dom.save(this.file);
+                } catch(e) {}
+            }
+        };
+
+        w.mbaShareCore = new UserData("local_storage");
+    })(window);
+
+    var _encode = (function() {
+        var escapeMap = {
+            "\b": "\\b",
+            "\t": "\\t",
+            "\n": "\\n",
+            "\f": "\\f",
+            "\r": "\\r",
+            '"': '\\"',
+            "\\": "\\\\"
+        };
+        function encodeString(source) {
+            if (/["\\\x00-\x1f]/.test(source)) {
+                source = source.replace(/["\\\x00-\x1f]/g,
+                    function(match) {
+                        var c = escapeMap[match];
+                        if (c) {
+                            return c
+                        }
+                        c = match.charCodeAt();
+                        return "\\u00" + Math.floor(c / 16).toString(16) + (c % 16).toString(16)
+                    })
+            }
+            return '"' + source + '"'
+        }
+        function encodeArray(source) {
+            var result = ["["],
+                l = source.length,
+                preComma,
+                i,
+                item;
+            for (i = 0; i < l; i++) {
+                item = source[i];
+                switch (typeof item) {
+                    case "undefined":
+                    case "function":
+                    case "unknown":
+                        break;
+                    default:
+                        if (preComma) {
+                            result.push(",")
+                        }
+                        result.push(_encode(item));
+                        preComma = 1
+                }
+            }
+            result.push("]");
+            return result.join("")
+        }
+        function pad(source) {
+            return source < 10 ? "0" + source: source
+        }
+        function encodeDate(source) {
+            return '"' + source.getFullYear() + "-" + pad(source.getMonth() + 1) + "-" + pad(source.getDate()) + "T" + pad(source.getHours()) + ":" + pad(source.getMinutes()) + ":" + pad(source.getSeconds()) + '"'
+        }
+        return function(value) {
+            switch (typeof value) {
+                case "undefined":
+                    return "undefined";
+                case "number":
+                    return isFinite(value) ? String(value) : "null";
+                case "string":
+                    return encodeString(value);
+                case "boolean":
+                    return String(value);
+                default:
+                    if (value === null) {
+                        return "null"
+                    } else {
+                        if (value instanceof Array) {
+                            return encodeArray(value)
+                        } else {
+                            if (value instanceof Date) {
+                                return encodeDate(value)
+                            } else {
+                                var result = ["{"],
+                                    encode = _encode,
+                                    preComma,
+                                    item;
+                                for (key in value) {
+                                    if (value.hasOwnProperty(key)) {
+                                        item = value[key];
+                                        switch (typeof item) {
+                                            case "undefined":
+                                            case "unknown":
+                                            case "function":
+                                                break;
+                                            default:
+                                                if (preComma) {
+                                                    result.push(",")
+                                                }
+                                                preComma = 1;
+                                                result.push(encode(key) + ":" + encode(item))
+                                        }
+                                    }
+                                }
+                                result.push("}");
+                                return result.join("")
+                            }
+                        }
+                    }
+            }
+        }
+    })();
+
+    var _decode = function(string, secure) {
+        if (typeof(string) != "string" || !string.length) {
+            return null
+        }
+        if (secure && !(/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(string.replace(/\\./g, "@").replace(/"[^"\\\n\r]*"/g, ""))) {
+            return null
+        }
+        try {
+            return eval("(" + string + ")")
+        } catch(e) {
+            return {}
+        }
+    }
+
+    var storageCore = {
+        setItem : function(key, value, expires) {
+            var data = {
+                'data' : value,
+                'expires' : expires !== undefined ? ( new Date().getTime() + ( parseInt(expires, 10) || 0 ) ) : 0
+            };
+            mbaShareCore.setItem(key, _encode(data));
+        },
+
+        getItem : function(key) {
+            var ret = null,
+                val = mbaShareCore.getItem(key);
+            if (val) {
+                val = _decode( val );
+                if ( val.expires ===0 ||  new Date().getTime() < val.expires ) {
+                    ret = val.data;
+                }
+                else {
+                    this.removeItem(key);
+                }
+            }
+
+            return ret;
+        },
+
+        removeItem : function(key) {
+            mbaShareCore.removeItem(key);
+        },
+
+        clear : function() {
+            mbaShareCore.clear();
+        }
+    };
+
+    MPing.tools || (MPing.tools = {});
+    MPing.tools.lstg =  storageCore;
+
+    try{
+        //上报localstg里面的click并上报
+        var mba_click = storageCore.getItem("mba_click");
+        if(mba_click){
+            var image = new Image(1,1);
+            image.src = mba_click;
+            storageCore.removeItem("mba_click");
+        }
+    } catch(e){}
+
 
 }(window));;/**
  * @fileoverview 这个文件是所有事件id与事件等级对应表
